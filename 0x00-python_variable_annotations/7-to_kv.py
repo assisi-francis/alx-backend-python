babc86251c04dc9dacca3a6a894ccabd57f6f1a4
assisi-francis <doncyco123@gmail.com>
2023-07-10 16:16:43 +0100

    task 9

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function to_kv
+"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a str, int or float and return str, float"""
+    return (k, float(v**2))
