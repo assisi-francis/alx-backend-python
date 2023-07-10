3b37d5d9ab4e665678cba7f4e762ab601d9fe13a
assisi-francis <doncyco123@gmail.com>
2023-07-10 23:51:20 +0100

    modify task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Type-annotated function floorwhich takes a float n as argument and returns the floor of the float.
+"""
+Type-annotated function floorwhich takes a float n as
+argument and returns the floor of the float.
 """
 
 
