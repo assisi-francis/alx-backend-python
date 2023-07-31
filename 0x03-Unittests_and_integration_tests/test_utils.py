62d86b487f90adf2383afaea4ed71f5000dce1c3
assisi-francis <doncyco123@gmail.com>
2023-07-31 23:41:11 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,15 +22,13 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-
-        expected_error_message = f"Key not found: '{path[-1]}'"
-        self.assertEqual(str(context.exception), expected_error_message)
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
